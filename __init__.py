8503643b3a9147dcf0c9a233c2e7965251804ed9
cooleydw494 <cooleydw494@gmail.com>
2023-05-22 00:44:29 -0400

    updated init file, LICENSE. Add fix-namespace script to help with module imports when >1 local repo.

--- __init__.py
+++ __init__.py
@@ -1,16 +1,18 @@
 """
-CodeMonkeys Extensible AI Automation Framework
+CodeMonkeys: An AI-focused Automation Tool & Extensible Framework
 """
 
 __version__ = '0.1.0'
-__author__ = 'Your Name'
+__author__ = 'David Wallace Cooley Jr'
 __email__ = 'your-email@example.com'
-__description__ = 'CodeMonkeys is an extensible framework for building Python applications.'
+__description__ = "CodeMonkeys is a highly configurable tool for creating/running strategic AI automations (and " \
+                  "more). Beyond that, its a thoughtful and extensible framework for creating automation tools. " \
+                  "CodeMonkeys' design was heavily influenced by how much it was used in its own development, " \
+                  "naturally converging to a sublime synergy of the tool and the framework."
 __year_created__ = '2023'
-__website__ = 'https://github.com/user/codemonkeys'
-__repository__ = 'https://github.com/user/codemonkeys.git'
-__maintainer__ = 'Your Name'
-__maintainer_email__ = 'your-email@example.com'
+__website__ = 'https://github.com/cooleydw494/code-monkeys'
+__repository__ = 'https://github.com/cooleydw494/code-monkeys.git'
+__maintainer__ = 'David Wallace Cooley Jr'
+__maintainer_email__ = 'cooleydw494@gmail.com'
 __dependencies__ = ['openai', 'pyyaml', 'python-dotenv', 'python-Levenshtein', 'termcolor', 'art']
-__licenses__ = ["Mozilla Public License 2.0"]
-
+__licenses__ = ["MIT"]
