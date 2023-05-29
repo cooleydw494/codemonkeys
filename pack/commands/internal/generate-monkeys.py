80dbd75fabbe714214c0e96667cbc5f56cfa91cf
cooleydw494 <cooleydw494@gmail.com>
2023-05-29 12:51:16 -0400

    remove some useless stuff, rewrite generate-monkeys

--- pack/commands/internal/generate-monkeys.py
+++ pack/commands/internal/generate-monkeys.py
@@ -1,22 +1,41 @@
 import os
+import shutil
+import time
+
 import yaml
 from definitions import MONKEYS_PATH
 
 
 def main():
-    # Create the directory if it doesn't exist
-    os.makedirs(MONKEYS_PATH, exist_ok=True)
-
     # Load the monkey configurations from the YAML file
     monkey_manifest = os.path.join(MONKEYS_PATH, "monkey-manifest.yaml")
     with open(monkey_manifest, "r") as f:
         monkeys = yaml.safe_load(f)
 
-    # Create the configuration files
+    # Create the directories and configuration files
     for monkey, config in monkeys.items():
-        with open(f"{MONKEYS_PATH}/{monkey}", "w") as f:
-            for key, value in config.items():
-                f.write(f"{key}='{value}'\n")
+        # Create the directory for the monkey if it doesn't exist
+        monkey_dir = os.path.join(MONKEYS_PATH, monkey)
+        os.makedirs(monkey_dir, exist_ok=True)
+
+        # Generate new config content
+        new_config_content = "\n".join(f"{key}='{value}'" for key, value in config.items()) + '\n'
+
+        config_file_path = os.path.join(monkey_dir, 'config')
+        # Check if new config content is different from the existing one
+        if os.path.exists(config_file_path):
+            with open(config_file_path, "r") as f:
+                existing_config_content = f.read()
+            if existing_config_content == new_config_content:
+                continue
+            else:
+                os.makedirs(os.path.join(monkey_dir, 'history'), exist_ok=True)
+                timestamp = time.strftime("%Y-%m-%d_%H:%M:%S")
+                shutil.move(config_file_path, os.path.join(monkey_dir, 'history', f'{timestamp}.config'))
+
+        # Write the config file
+        with open(os.path.join(monkey_dir, 'config'), "w") as f:
+            f.write(new_config_content)
 
 
 if __name__ == "__main__":
