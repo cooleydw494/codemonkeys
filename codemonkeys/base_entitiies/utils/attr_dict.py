4ddadaa4e331255df3724c5f89caa1ce6320b40f
cooleydw494 <cooleydw494@gmail.com>
2023-07-02 00:45:31 -0400

    fix typo in command class, remove attr_dict.py

--- codemonkeys/base_entitiies/utils/attr_dict.py
+++
@@ -1,7 +0,0 @@
-class AttrDict(dict):
-    """
-    A custom dictionary class that allows attribute-like access.
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__dict__ = self
