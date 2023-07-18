d97672d3140e4b72c58a56c055230a1518c1feee
cooleydw494 <cooleydw494@gmail.com>
2023-07-18 00:06:09 -0400

    add logging for output checker result on fail

--- codemonkeys/composables/output_checker.py
+++ codemonkeys/composables/output_checker.py
@@ -46,7 +46,7 @@ class OutputChecker:
             print_t(f"Output Check Passed. {check_result}", 'special')
             output_valid = True
         else:
-            print_t("Output Check Failed.", 'warning')
+            print_t(f"Output Check Failed. Result: {check_result}", 'warning')
             output_valid = False
 
         self.current_try += 1
