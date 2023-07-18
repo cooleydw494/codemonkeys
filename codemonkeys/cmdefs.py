459e5bfa6d102ac1fbbd562d11638487fa810616
cooleydw494 <cooleydw494@gmail.com>
2023-07-18 00:31:00 -0400

    update cmdefs with metamonkeys' comment-monkey

--- codemonkeys/cmdefs.py
+++ codemonkeys/cmdefs.py
@@ -1,18 +1,20 @@
 import os
 
-from pkg_resources import resource_filename
-
+# Version of the module
 VERSION = '0.2.5'
 
-"""  CORE PATHS
+"""
+CORE PATHS
 
 This is a framework-level PATH definitions file.
 It is separate from defs.py for usage in monk-new, when there is no project ROOT_PATH.
 It is also used anywhere else a framework-level path is needed.
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  """
+"""
 
+# Getting the root directory of the current script
 CM_ROOT_PATH = os.path.dirname(__file__)
 
+# Define important directory paths rooted in CM_ROOT_PATH
 CM_AUTOMATIONS_PATH = os.path.join(CM_ROOT_PATH, "automations")
 CM_BARRELS_PATH = os.path.join(CM_ROOT_PATH, "barrels")
 CM_COMPOSABLES_PATH = os.path.join(CM_ROOT_PATH, "composables")
@@ -22,16 +24,18 @@ CM_CONFIG_PATH = os.path.join(CM_ROOT_PATH, "config")
 CM_HELP_PATH = os.path.join(CM_ROOT_PATH, "help")
 CM_STOR_PATH = os.path.join(CM_ROOT_PATH, "stor")
 
+# Define specific file paths in CM_CONFIG_PATH
 CM_ENV_CLASS_PATH = os.path.join(CM_CONFIG_PATH, "env_class.py")
 CM_MONKEY_CONFIG_CLASS_PATH = os.path.join(CM_CONFIG_PATH, "monkey_config_class.py")
 CM_THEME_CONFIG_PATH = os.path.join(CM_CONFIG_PATH, 'theme.py')
 
-
+# Define sub-directory paths in CM_STOR_PATH
 CM_STOR_TEMP_PATH = os.path.join(CM_STOR_PATH, "temp")
 CM_STOR_MONK_PATH = os.path.join(CM_STOR_PATH, "monk")
 CM_STOR_DEFAULTS_PATH = os.path.join(CM_STOR_PATH, "defaults")
 CM_STOR_SNIPPETS_PATH = os.path.join(CM_STOR_PATH, "snippets")
 
+# Define default file paths in CM_STOR_DEFAULTS_PATH
 CM_ENV_DEFAULT_PATH = os.path.join(CM_STOR_DEFAULTS_PATH, ".env.default")
 CM_MONKEY_MANIFEST_DEFAULT_PATH = os.path.join(CM_STOR_DEFAULTS_PATH, "monkey-manifest.yaml")
 CM_MONKEY_CONFIG_DEFAULTS_DEFAULT_PATH = os.path.join(CM_STOR_DEFAULTS_PATH, "monkey-config-defaults.yaml")
@@ -41,6 +45,7 @@ CM_GITIGNORE_DEFAULT_PATH = os.path.join(CM_STOR_DEFAULTS_PATH, '.default-gitign
 CM_DEFAULT_REQUIREMENTS_PATH = os.path.join(CM_STOR_DEFAULTS_PATH, 'default-requirements.txt')
 CM_README_DEFAULT_PATH = os.path.join(CM_STOR_DEFAULTS_PATH, 'DEFAULT-README.md')
 
+# Define example entity file paths in CM_STOR_PATH
 CM_ENTITY_EXAMPLES_PATH = os.path.join(CM_STOR_PATH, "entity_examples")
 CM_EXAMPLE_COMMAND_PATH = os.path.join(CM_ENTITY_EXAMPLES_PATH, "example-command.py")
 CM_EXAMPLE_AUTOMATION_PATH = os.path.join(CM_ENTITY_EXAMPLES_PATH, "example-automation.py")
