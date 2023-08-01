1557feaf8e0eb38d1a0c70a72e493740bd8caeb1
cooleydw494 <cooleydw494@gmail.com>
2023-07-31 22:28:36 -0400

    CodeMonkeys notation update

--- codemonkeys/utils/git/gitter.py
+++ codemonkeys/utils/git/gitter.py
@@ -5,34 +5,94 @@ from codemonkeys.utils.git.gitter_error import GitterError
 
 
 class Gitter:
+    """
+    A composable class used to interact with git repos.
+
+    :param str repo_path: a string representing the path to the git repo.
+    """
 
     def __init__(self, repo_path: str):
+        """
+        :param str repo_path: The path to the repo.
+        """
         self.repo_path = repo_path
 
     def clone(self, repo_url: str) -> str:
+        """
+        Clones the repo at the provided URL.
+
+        :param str repo_url: The URL of the repo to clone.
+        :return: The output of the git command.
+        """
         return self._run_git_command(["clone", repo_url])
 
-    def pull(self, remote: str = "origin", branch: str = "master") -> str:
+    def pull(self, remote: str = "origin", branch: str = "main") -> str:
+        """
+        Pull the latest changes from the specified remote and branch.
+
+        :param str remote: The name of the remote repo. Default: "origin".
+        :param str branch: The name of the branch to pull from. Default: "main".
+        :return: The output of the git command.
+        """
         return self._run_git_command(["pull", remote, branch])
 
-    def push(self, remote: str = "origin", branch: str = "master") -> str:
+    def push(self, remote: str = "origin", branch: str = "main") -> str:
+        """
+        Push committed changes to the specified remote and branch.
+
+        :param str remote: The name of the remote repo. Default: "origin".
+        :param str branch: The name of the branch to push to. Default: "main".
+        :return: The output of the git command.
+        """
         return self._run_git_command(["push", remote, branch])
 
     def commit(self, message: str, add_all: bool = False) -> str:
+        """
+        Commit changes with the provided message.
+        If the add_all flag is True, stages all changes.
+
+        :param str message: The commit message.
+        :param bool add_all: Whether to stage all changes. Default: False.
+        :return: The output of the git command.
+        """
         if add_all:
             self._run_git_command(["add", "-A"])
         return self._run_git_command(["commit", "-m", message])
 
     def status(self) -> str:
+        """
+        Returns the status of the current repo.
+
+        :return: The status of the current repo.
+        """
         return self._run_git_command(["status"])
 
     def checkout(self, branch_name: str) -> str:
+        """
+        Switch to the specified branch.
+
+        :param str branch_name: The name of the branch to switch to.
+        :return: The output of the git command.
+        """
         return self._run_git_command(["checkout", branch_name])
 
     def create_branch(self, branch_name: str) -> str:
+        """
+        Create a new branch with the provided branch name.
+
+        :param str branch_name: The name of the new branch.
+        :return: The output of the git command.
+        """
         return self._run_git_command(["branch", branch_name])
 
     def _run_git_command(self, command: List[str]) -> str:
+        """
+        Runs the provided git command and returns the result.
+
+        :param List[str] command: The git command.
+        :return: The output of the git command.
+        :raise GitterError: If the git command fails.
+        """
         try:
             result = subprocess.run(
                 ["git"] + command,
--- codemonkeys/utils/git/gitter_error.py
+++ codemonkeys/utils/git/gitter_error.py
@@ -2,13 +2,35 @@ from typing import List
 
 
 class GitterError(Exception):
+    """
+    Custom exception for Gitter errors.
+
+    :param List[str] command: A list of command tokens.
+    :param int returncode: The exit status of the command.
+    :param str stdout: The output from the command.
+    :param str stderr: The error message from the command.
+    """
+
     def __init__(self, command: List[str], returncode: int, stdout: str, stderr: str):
+        """
+        Initialize GitterError with command details, return code and system output.
+        
+        :param List[str] command: A list of command tokens.
+        :param int returncode: The exit status of the command.
+        :param str stdout: The output from the command.
+        :param str stderr: The error message from the command.
+        """
         self.command = command
         self.returncode = returncode
         self.stdout = stdout
         self.stderr = stderr
 
     def __str__(self) -> str:
+        """
+        Return a formatted string with error information.
+
+        :return: The string representation of the GitterError.
+        """
         return (
             f"Git command '{' '.join(self.command)}' failed with return code {self.returncode}.\n"
             f"Output: {self.stdout}\n"
