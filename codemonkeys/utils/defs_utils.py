b54f2bdb853c713f2bb443c5fc159619f31234c1
cooleydw494 <cooleydw494@gmail.com>
2023-07-31 22:34:14 -0400

    CodeMonkeys notation update

--- codemonkeys/utils/defs_utils.py
+++ codemonkeys/utils/defs_utils.py
@@ -1,6 +1,5 @@
 import importlib.util
 import os
-import subprocess
 
 import Levenshtein
 from termcolor import colored
@@ -12,74 +11,32 @@ It houses code that can be run without importing other project modules (which wo
 NO PROJECT MODULES, CONFIGS, ETC SHOULD BE IMPORTED HERE.
 """
 
-# Lists of python and pip commands to be checked
-python_commands = ["python3", "python3.11", "python3.10", "python3.9", "python3.8", "python3.7", "python3.6", "python"]
-
-
-def get_python_command() -> str:
-    # Loop over the python_commands list and return the first valid command
-    for cmd in python_commands:
-        if _test_command(cmd):
-            return cmd
-    print("No valid python command is available. Please add one of these to your path, "
-          "or set the PYTHON_COMMAND manually in defs.py")
-
-
-def _test_command(command: str) -> bool:
-    # noinspection PyBroadException
-    try:
-        # Use --version flag to check if a command is present
-        # It should throw an Exception if it isn't
-        output = subprocess.check_output([command, "--version"])
-        return True
-    except Exception:
-        return False
-
 
 def find_project_root() -> str:
-    """Find the root directory of the project (i.e., the closest parent directory containing a `.env` file)."""
-    cwd = os.getcwd()
+    """
+    Find the root directory of the project (i.e., the closest parent directory containing a `.env` file).
 
+    :return: The project root directory path.
+    """
+    cwd = os.getcwd()
     while cwd != os.path.dirname(cwd):  # Stop when we reach the root directory
         if '.env' in os.listdir(cwd):
-            # check for existence of root/config/monkey-manifest.yaml to verify its a codemonkeys project
             if os.path.exists(os.path.join(cwd, 'config', 'monkey-manifest.yaml')):
                 return cwd
             else:
                 print(colored("You must run `monk` within a CodeMonkeys project", 'red'))
                 exit(1)
         cwd = os.path.dirname(cwd)
-
     print(colored("Could not find project root", 'red'))
     exit(1)
 
 
-def import_class_from_path_with_fallback(path: str, class_name: str, fallback_class: type) -> type:
-    """
-    Try to import a class from a Python file at a given path. If the import fails, return a fallback class.
-
-    Args:
-    - path (str): The path to the Python file.
-    - class_name (str): The name of the class to import.
-    - fallback_class (type): The class to use if the import fails.
-
-    Returns:
-    - The imported class, or the fallback class if the import fails.
-    """
-    try:
-        spec = importlib.util.spec_from_file_location(class_name, path)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        return getattr(module, class_name)
-    except (ImportError, FileNotFoundError, AttributeError) as e:
-        print(colored(f"Could not import {class_name} from {path}", 'red'))
-        print(colored(f"Using fallback class {fallback_class.__name__}", 'yellow'))
-        print(colored(f"Error: {e}", 'red'))
-        return fallback_class
-
-
 def levenshtein_distance(str1: str, str2: str) -> int:
-    # determine if string1 or string2 fully contains the other and if so print(0) to emulate a match
-    if str1 in str2 or str2 in str1:
-        return 0
-    return Levenshtein.distance(str1, str2)
+    """
+    Calculate the Levenshtein distance between two strings.
+
+    :param str str1: The first string for comparison.
+    :param str str2: The second string for comparison.
+    :return: The Levenshtein distance between the two strings.
+    """
+    return Levenshtein.distance(str1.strip(), str2.strip())
--- codemonkeys/utils/file_ops.py
+++ codemonkeys/utils/file_ops.py
@@ -2,6 +2,12 @@ import os
 
 
 def get_file_contents(path: str) -> str:
+    """
+    Reads the contents of a file and returns it as a str.
+
+    :param str path: The path to the file.
+    :return: The contents of the file.
+    """
     path = resolve_file_path(path)
 
     if not os.path.isfile(path):
@@ -14,6 +20,12 @@ def get_file_contents(path: str) -> str:
 
 
 def write_file_contents(path: str, contents: str) -> None:
+    """
+    Writes the specified contents to a file.
+
+    :param str path: The path to the file.
+    :param str contents: New file contents
+    """
     path = resolve_file_path(path)
 
     with open(path, "w") as f:
@@ -21,9 +33,21 @@ def write_file_contents(path: str, contents: str) -> None:
 
 
 def file_exists(path: str) -> bool:
+    """
+    Checks if a file exists at the specified path.
+
+    :param str path: The path of the file.
+    :return: True if the file exists, otherwise False.
+    """
     path = resolve_file_path(path)
     return os.path.isfile(path)
 
 
 def resolve_file_path(path: str) -> str:
+    """
+    Expands a file path that may contain user shortcuts (e.g. ~).
+
+    :param str path: The path to be resolved
+    :return: The expanded file path
+    """
     return os.path.expanduser(path)
