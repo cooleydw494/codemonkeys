4733dbd6f0eb808e0e2338002d50f4a91d07772b
cooleydw494 <cooleydw494@gmail.com>
2023-08-02 00:47:13 -0400

    add back sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ release = '0.3.0'
 extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -28,5 +29,5 @@ autodoc_mock_imports = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
