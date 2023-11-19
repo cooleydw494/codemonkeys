efeb5cdb539479ad648c70beefd260c232f9408c
cooleydw494 <cooleydw494@gmail.com>
2023-11-19 13:31:29 -0500

    make some sphinx config changes to hopefully produce somewhat better docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx_rtd_theme',
+    'sphinx.ext.viewcode',
 ]
 autosummary_generate = True
 templates_path = ['templates']
@@ -40,7 +41,7 @@ autodoc_default_options = {
     'undoc-members': True,
     'private-members': True,
     'show-inheritance': True,
-
+    'inherited-members': True,  # Added to include inherited members in documentation
 }
 
 # -- Options for HTML output -------------------------------------------------
