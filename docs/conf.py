a82840cbd68b3481d13a909c85a24f331c11f73c
cooleydw494 <cooleydw494@gmail.com>
2023-11-30 00:34:44 -0500

    Bump version: 1.0.4 → 1.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ import sys
 project = 'codemonkeys'
 copyright = '2023, David Cooley'
 author = 'David Cooley'
-release = '1.0.4'
+release = '1.0.5'
 
 sys.path.insert(0, os.path.abspath('../codemonkeys'))
 
